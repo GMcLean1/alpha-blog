d73e516b30611d53d9acf9abd0d5e3d4f18a10db
Gary McLean <Gary.M.McLean@Dartmouth.edu>
2020-04-17 10:55:42 -0400

    Add one to many associations between users and articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_17_130214) do
+ActiveRecord::Schema.define(version: 2020_04_17_142956) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
